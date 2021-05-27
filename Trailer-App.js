1cecffa2173f17963536cf73b02e14bf02f6e0a6
oussema-yahyaoui <ousema.benabdallah.yahyaoui@gmail.com>
2021-05-27 16:01:40 +0100

    step two login done

--- Trailer-App.js
+++ Trailer-App.js
@@ -1,1 +1,10 @@
-console.log('hello')
+
+$('#idsign').click(function () {
+    var user = localStorage.getItem("user");
+    var pass = localStorage.getItem("password")
+    if (user === $('#idname').val() && pass === $('#idpassword').val()) {
+        return alert("login successful")
+    } else {
+        return alert("username or password is incorrect")
+    }
+})
